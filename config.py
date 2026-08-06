1b6e2f75b23f635991133c5f9d40a36e1e13c6a4
hello-botcmd <swaruppanda2008@gmail.com>
2026-08-06 12:12:31 +0530

    Upload config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+#---------- © sᴛᴀʟᴋᴇʀ@hehe_stalker
+#---------- ᴘʀᴏJᴇᴄᴛ - ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛ sᴇʟʟɪɴɢ ʙᴏᴛ
+#-------------------------------------------------------
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def _getenv(name: str, default: str | None = None, required: bool = False) -> str:
+    val = os.getenv(name, default)
+    if required and (val is None or val == ""):
+        raise RuntimeError(f"Missing required env var: {name}")
+    return val
+    
+MUST_JOIN_CHANNEL = "@TgAltSupport"
+BOT_TOKEN = _getenv("BOT_TOKEN", required=True)
+ADMIN_IDS = [int(i) for i in _getenv("ADMIN_IDS", "7813102548", required=True).replace(" ", "").split(",") if i]
+API_ID = "21377358"
+API_HASH = "e05bc1f4f03839db7864a99dbf72d1cd"
+
+DATABASE_URL = _getenv("DATABASE_URL", "mongodb+srv://arshtyagi007_db_user:arshtyagi@cluster0.iymlgmx.mongodb.net/?appName=Cluster0")
+
+
+
+DEFAULT_CURRENCY = _getenv("DEFAULT_CURRENCY", "₹")
+MIN_BALANCE_REQUIRED = float(_getenv("MIN_BALANCE_REQUIRED", "0"))
